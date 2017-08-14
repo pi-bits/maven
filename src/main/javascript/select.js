ebbbc4919d41753eb1289ac360f269d53a89ded9
Prashant Naik <Prashant Naik>
2017-08-14 07:42:30 +0100

    jasmine specs added

---
+++ src/main/javascript/select.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".cars").css("background-color","yellow");
+
+    
+ $(".cars").on('change',function(){
+ 	console.log(this.value);
+ });
+
+
+});
---
+++ src/test/javascript/select.spec.js
@@ -0,0 +1,9 @@
+describe("A suite", function() {
+	beforeEach(function(){
+
+	});
+	
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
