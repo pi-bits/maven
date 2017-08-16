aad011028b8cc056492290d8c1e681b9aee5f6db
Prashant Naik <Prashant Naik>
2017-08-16 07:56:49 +0100

    phantoms js driver added

---
+++ src/test/java/com/serviceinfotech/HTMLUnitDriverDemo.java
@@ -0,0 +1,19 @@
+package com.serviceinfotech;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+public class HTMLUnitDriverDemo {
+    @Test
+    public void testGooglePageWithHTMLUnitDriver() throws Exception {
+
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("https://www.google.co.uk");
+        Thread.sleep(1000);
+        Assert.assertThat("Google", CoreMatchers.is(CoreMatchers.equalTo(driver.getTitle())));
+    }
+}
--- src/test/java/com/serviceinfotech/PhantomJsDemoTest.java
+++ src/test/java/com/serviceinfotech/PhantomJsDemoTest.java
@@ -7,12 +7,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
 
 public class PhantomJsDemoTest {
     @Test
     public void testGooglePageWithPhantomJS() throws Exception {
+
         File file = new File("C:\\SWF\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
-        System.setProperty("phantomjs.binary.path", file.getAbsolutePath());
+//        URL location = getClass().getProtectionDomain().getCodeSource().getLocation();
+//  location.getPath();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = classLoader.getResource("phantomjs/bin/"+"phantomjs.exe");
+
+        System.setProperty("phantomjs.binary.path", resource.getPath());
         WebDriver driver = new PhantomJSDriver();
         driver.get("https://www.google.co.in");
         Thread.sleep(1000);
